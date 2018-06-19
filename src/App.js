5933fff914e2ccdd05d6c835419a47324f62a0ff
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-06-19 12:00:55 +0200

    divided the component aplication and created scss and js files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Header from "./js/header";
+import CompanyForm from "./js/companyForm";
+import NewRecordForm from "./js/newRecordForm";
+import MainTable from "./js/mainTable";
+import Footer from "./js/footer";
+import main from "./scss"
+
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Header></Header>
+            <CompanyForm></CompanyForm>
+            <NewRecordForm></NewRecordForm>
+            <MainTable></MainTable>
+            <Footer></Footer>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/js/header.js
@@ -0,0 +1,12 @@
+
+
+class Header extends React.Component{
+    render() {
+        return (
+            <div>
+                <div className="mainLogo"></div>
+                <div></div>
+            </div>
+        )
+    }
+}

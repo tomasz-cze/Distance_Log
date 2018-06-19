06975028baed0ae4ecf299227805e9c42d84fd0e
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-06-19 14:59:17 +0200

    created table header and mainTable html structure

--- src/js/mainTable.js
+++ src/js/mainTable.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react';
 class MainTable extends Component{
     render(){
         return (
-            <div>
-
+            <div className="mainTable container">
+                <div>
+                    <div className="printHeader"></div>
+                </div>
+                <div className="table">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>LP.</th>
+                                <th>Data</th>
+                                <th>Dzień tygodnia (niewidoczny przy druku)</th>
+                                <th>Trasa</th>
+                                <th>Cel wyjazdy</th>
+                                <th>Ilość km</th>
+                                <th>Stawka/km</th>
+                                <th>Wartość</th>
+                                <th>Uwagi</th>
+                                <th>Opcje</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
